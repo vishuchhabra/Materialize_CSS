78eff4fc3fa7214b7f92cbda63334c71aa57b17a
vishuchhabra <vishuchhabra1016@gmail.com>
2020-06-22 10:30:56 +0530

    All Done

---
+++ js/Loader.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.modal').modal()
+    $('.parallax').parallax()
+    $('.sidenav').sidenav()
+    $('.slider').slider({
+        fullWidth: true
+      });
+
+    $('.parallax').parallax();
+    $('.myreviews').carousel(
+        {
+            numVisible:7,
+            shift:55,
+            padding:55
+        }
+    )
+
+})
+
+function toggleModal(){
+    var instance = M.Modal.getInstance($('#modal3'))
+    instance.open()
+}
